8412040d49a1dbeb73e436f592874dd9ff595ffa
Benjamin L. Miles <bmiles@nethealthshops.com>
2021-09-24 09:21:00 -0500

    refactored var names to be more desc

--- main.go
+++ main.go
@@ -15,26 +15,26 @@ func main() {
 		println(err)
 		return
 	}
-	githubtoken := string(os.Getenv("githubPAT"))
+	githubToken := string(os.Getenv("githubPAT"))
 	githubTarget := string(os.Getenv("githubTarget"))
 	gitlabNamespaceID := string(os.Getenv("gitlabNamespaceID"))
 	gitlabToken := string(os.Getenv("gitlabToken"))
 	ctx := context.Background()
 	StaticTokenSource := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: githubtoken},
+		&oauth2.Token{AccessToken: githubToken},
 	)
 
 	newClient := oauth2.NewClient(ctx, StaticTokenSource)
 	client := github.NewClient(newClient)
-	repos, _, err := client.Repositories.ListByOrg(ctx, githubTarget, nil)
+	repositories, _, err := client.Repositories.ListByOrg(ctx, githubTarget, nil)
 	if err != nil {
 		println(err)
 		return
 	}
 	// For each GitHub Repo Within the target Org
 	// Create a GitLab Repo
-	for i := 0; i < len(repos); i++ {
-		repo := *repos[i].Name
+	for i := 0; i < len(repositories); i++ {
+		repo := *repositories[i].Name
 		payload := gitlab.CreateRepoPayload{
 			NamespaceID: gitlabNamespaceID,
 			Name:        repo,
